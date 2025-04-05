cd11db3ca169fabe66ed62c1c978399176d8ced1
Pratham Chouhan <109415931+chouhanpratham@users.noreply.github.com>
2025-04-05 22:47:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,14 +13,11 @@ resource "azurerm_service_plan" "plan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Windows"
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  sku_name            = "B1"    # Basic plan with 1 instance
 }
 
 resource "azurerm_windows_web_app" "app" {
-  name                = "webapijenkinspratham2222225"
+  name                = var.as_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.plan.id
@@ -34,12 +31,10 @@ resource "azurerm_windows_web_app" "app" {
   }
 
   app_settings = {
-    WEBSITE_NODE_DEFAULT_VERSION = "~20"
-    SCM_DO_BUILD_DURING_DEPLOYMENT = "false"
-    WEBSITE_RUN_FROM_PACKAGE = "1"
+    WEBSITE_NODE_DEFAULT_VERSION       = "~20"
+    SCM_DO_BUILD_DURING_DEPLOYMENT    = "false"
+    WEBSITE_RUN_FROM_PACKAGE          = "1"
   }
 }
-
-
 
 
